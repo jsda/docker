55f6b9e44e74da41d0a28fcbae9f269e29242abe
jsda <jsda@github.com>
2020-05-12 22:42:44 +0800

    20200512-2242

---
+++ wine/wine.sh
@@ -0,0 +1,7 @@
+git clone --depth 1 https://github.com/jsda/docker-wine-linux.git ../wine && cd ../wine
+svn co --force -q https://github.com/RokasUrbelis/docker-wine-linux/trunk/deb ../deb && svn revert -R ../deb && rm -rf deb/ && mv ../deb ./ && echo "deb更新成功" || echo "deb更新失败"
+docker buildx build --push \
+  --platform ${PLATFORMS} \
+  -t ${IMAGE_NAME}:${IMAGE_TAG} \
+  -f ${DOCKERFILE} \
+  ${FILE_PATH}
